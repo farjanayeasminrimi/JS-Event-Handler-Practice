190ca2fbd97e8389720d500f9277d4d05c42dccc
Farjana Yeasmin Rimi <farjanayeasmin003@gmail.com>
2026-02-17 23:24:30 +0600

    applied other event hander

---
+++ JS/delete.js
@@ -0,0 +1,25 @@
+const deleteBtn = document.getElementById("delete-btn");
+const deleteInput = document.getElementById("delete-input");
+const main = document.getElementById("main");
+deleteInput.addEventListener("mouseenter", function () {
+  deleteInput.style.backgroundColor = "gold";
+});
+
+deleteInput.addEventListener("mousemove", function () {
+  // console.log("moved");
+});
+
+deleteBtn.addEventListener("mouseout", function () {
+  main.style.backgroundColor = "blue";
+});
+
+deleteInput.addEventListener("focus", function () {
+  deleteInput.style.borderColor = "red";
+  deleteInput.style.outline = "none";
+});
+
+deleteInput.addEventListener("blur", function () {
+  if (deleteInput.value.length < 8) {
+    alert("Password must be 8 character");
+  }
+});
