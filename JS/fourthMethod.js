03aa3af4118bd1c66c3b65d624f8b60f095cff2f
Farjana Yeasmin Rimi <farjanayeasmin003@gmail.com>
2026-02-17 13:34:16 +0600

    applied fourth  method of onclick

---
+++ JS/fourthMethod.js
@@ -0,0 +1,5 @@
+const fourthBtn = document.getElementById("fourth-btn");
+fourthBtn.addEventListener("click", function () {
+  const fourthSection = document.querySelector("#fourth-Section");
+  fourthSection.style.backgroundColor = "yellow";
+});
