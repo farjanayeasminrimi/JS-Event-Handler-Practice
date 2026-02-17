aebca3d8cc2ca729abf6538ae78951ff5a831a57
Farjana Yeasmin Rimi <farjanayeasmin003@gmail.com>
2026-02-17 12:48:05 +0600

    applied third method of onclick

---
+++ JS/thirdMethod.js
@@ -0,0 +1,5 @@
+const btn3rd = document.getElementById("button-3rd");
+function colorChange() {
+  const sectionThree = document.querySelector("#section-3");
+  sectionThree.style.backgroundColor = "red";
+}
